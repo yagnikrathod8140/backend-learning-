12cd350339d5cc9bd2b92f55bce3ae2e8428d136
Yagnik-Nexotips <yagnik90r@gmail.com>
2025-04-02 11:12:00 +0530

    initially commit

---
+++ scripte.js
@@ -0,0 +1,16 @@
+// NPM unuderstanding
+
+// npm - node package manager
+
+
+// installing and uninstalling anything basics & advanced
+    // npm i packej ka name
+    // npm uninstall packej ka name
+    // installing particular varsions
+
+// unuderstanding node_modules
+
+// dependencies = packages and packages ki dependencies
+// devdependencies = aise packages jis ko ham sirkh devlopmet ke vakt use karte he
+
+// scripts - unuderstanding default scripts PATH and custom scripts
