244c3c4677e063dbaa8153c5b89258914d4b29ca
Yagnik-Nexotips <yagnik90r@gmail.com>
2025-04-10 11:27:01 +0530

    video-6 over

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine','ejs');
 
 app.get("/", function(req, res){
-    res.send("chal raha hai");
+    res.render("index");
+});
+app.get("/profile/:username", function(req, res){
+    res.send(`welcome ${req.params.username}`);
+});
+
+app.get("/profile/:username/:age", function(req, res){
+    res.send(`welcome ${req.params.username} of age ${req.params.age}`);
 });
 
 app.listen(3000, function(){
-    console.log("it runing");
+    console.log("it's runing");
 })
