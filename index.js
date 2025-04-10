efd1fc454cae3d69c131b1ef278434b73f95a491
Yagnik-Nexotips <yagnik90r@gmail.com>
2025-04-10 15:56:31 +0530

    done

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const path = require('path')
+const express = require('express')
 const app = express();
+const path = require('path')
+const fs = require('fs');
+const { log } = require('console');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname,"public")))
 
-app.get("/", function(req, res){
-    res.render("index");
-});
-app.get("/profile/:username", function(req, res){
-    res.send(`welcome ${req.params.username}`);
-});
-
-app.get("/profile/:username/:age", function(req, res){
-    res.send(`welcome ${req.params.username} of age ${req.params.age}`);
-});
-
-app.listen(3000, function(){
-    console.log("it's runing");
+app.get('/', function(req, res){
+    fs.readdir(`./files`,function(err, files){
+        console.log(files)
+        res.render("index", {files: files});
+    })
 })
+
+app.post('/create', function(req, res){
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+    res.redirect("/")
+   });
+})
+
+app.listen(3000);
