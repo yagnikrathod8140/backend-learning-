5d53693a38e21df21538dd147a1a4d394f1f6deb
Yagnik-Nexotips <yagnik90r@gmail.com>
2025-04-11 10:29:08 +0530

    video-9 done

--- index.js
+++ index.js
@@ -23,8 +23,14 @@ app.get('/file/:filename', function(req, res){
 })
 
 app.get('/edit/:filename', function(req, res){
-   res.render('edit');
+   res.render('edit',{filename: req.params.filename});
 })
+
+app.post('/edit', function(req, res){
+   fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+      res.redirect("/")
+   })
+});
 
 app.post('/create', function(req, res){
    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
